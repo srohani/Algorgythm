f1fe4dc207cb6acb0cd97d99a830ac9d92e2848f
srohani <SamerRohani@Samers-MacBook-Pro.local>
2015-09-20 03:41:56 -0700

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Samer Rohani on 2015-09-20.
+//  Copyright (c) 2015 Samer Rohani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Algorhythm
+//
+//  Created by Samer Rohani on 2015-09-20.
+//  Copyright (c) 2015 Samer Rohani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
